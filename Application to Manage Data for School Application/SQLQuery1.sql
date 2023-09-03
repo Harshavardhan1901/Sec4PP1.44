c4899ae59053996d06677ce0c7ab968c8e1a49f8
harsha1901@yahoo.com <harsha1901@yahoo.com>
2023-09-03 20:37:31 +0530

    Assignment

---
+++ Application to Manage Data for School Application/SQLQuery1.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE SchoolDb
+use SchoolDb
+CREATE TABLE Classes (
+    class_id INT PRIMARY KEY,
+    class_name VARCHAR(50) NOT NULL
+)
+
+insert into Classes values (1,'10th'),(2,'8th')
+
+CREATE TABLE Subject (
+    subject_id INT PRIMARY KEY,
+    subject_name VARCHAR(50) NOT NULL,
+    class_id INT,
+    FOREIGN KEY (class_id) REFERENCES Classes (class_id)
+)
+insert into Subject values (1,'Science',2),(2,'Mathematics',1) 
+
+CREATE TABLE Student (
+    student_id INT PRIMARY KEY,
+    student_name VARCHAR(50) NOT NULL,
+    class_id INT,
+    subject_id INT,
+    FOREIGN KEY (class_id) REFERENCES Classes (class_id),
+    FOREIGN KEY (subject_id) REFERENCES Subject (subject_id)
+)
+
+insert into Student values (1,'Sam',2,1),(2,'Raj',1,2)
+
+select * from Student
+
